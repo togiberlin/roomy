aead98c1a3ce7d9383efdef5a053586ceaf8e6cb
Togi Dashnyam <togi.berlin@gmail.com>
2017-06-08 14:13:42 +0200

    Added review controller and routes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,6 +8,9 @@ class RoomsController < ApplicationController
 
   def show
     @photos = @room.photos
+    @booked = Reservation.where('room_id = ? AND user_id = ?', @room.id, current_user.id).present? if current_user
+    @reviews = @room.reviews
+    @has_review = @reviews.find_by(user_id: current_user.id) if current_user
   end
 
   def new
