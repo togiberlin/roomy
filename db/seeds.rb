13a4c7c513491eb3a797f9272660bb91caa32fd6
Togi Dashnyam <togi.berlin@gmail.com>
2017-06-10 17:49:09 +0200

    Added to do note inside seedfile. Default users, rooms and room photos should be created to save time

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# To Do: Add seeds for users
+
+# To Do: Add seeds for rooms
+
+# To Do: Add seeds for room photos
