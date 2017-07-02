856d58e2ff08cfadf4dc0c879836e0f3eb4f8af9
Togi Dashnyam <togi.berlin@gmail.com>
2017-07-02 11:55:53 +0200

    Small bugfix: fixed room model validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,14 +5,13 @@ class Room < ActiveRecord::Base
   has_many :reviews
 
   geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   validates_presence_of :home_type, :room_type, :accommodate, :bed_room,
-                        :bath_room, :listing_name, :summary, :address, 
-                        :latitude, :longitude
+                        :bath_room, :listing_name, :summary, :address
   validates_length_of :listing_name, maximum: 50
   validates_length_of :summary, maximum: 500
   validates_length_of :address, maximum: 100
-  after_validation :geocode, if: :address_changed?
 
   def average_rating
     reviews.count == 0 ? 0 : reviews.average(:star).round(2)
